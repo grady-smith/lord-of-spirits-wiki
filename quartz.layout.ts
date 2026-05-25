bfafc3e0bfe44da520d0413785e17cffc93ea47e
Steve <steve@example.com>
2026-05-25 10:20:49 -0400

    Fix transcript titles, timestamps, and pipeline bugs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,8 +33,11 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
-  ],
+    Component.Explorer({
+      mapFn: (node) => {
+        node.displayName = node.name
+      },
+    }),
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
@@ -57,7 +60,11 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      mapFn: (node) => {
+        node.displayName = node.name
+      },
+    }),
   ],
   right: [],
 }
